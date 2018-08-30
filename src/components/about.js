4502d4bc55c3d3a7e8e091254c99bb730ab83c54
John Carlisle <jmcarlisle@live.com>
2018-08-29 17:58:54 -0700

    setup complete

---
+++ src/components/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props => (
+    <div className ="center">
+        <h1> About Us </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati ab asperiores voluptatem consequuntur earum blanditiis perferendis unde esse. At earum rem nisi dolor, accusamus, laboriosam excepturi quia doloremque mollitia atque temporibus cupiditate!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+);
