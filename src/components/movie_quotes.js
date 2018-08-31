f88a6db0f48c4ee892c5185854f4a8e4e1692b56
John Carlisle <jmcarlisle@live.com>
2018-08-31 12:59:00 -0700

    added auth to component

--- src/components/movie_quotes.js
+++ src/components/movie_quotes.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
+import auth from '../hoc/auth';
+
+
 
 class MovieQuotes extends Component {
     render(){
+
         return (
             <div>
                 <h1 className ="center">Movie Quote!</h1>
@@ -11,4 +15,4 @@ class MovieQuotes extends Component {
         )
     }
 }
-export default MovieQuotes;
+export default auth(MovieQuotes);
---
+++ src/hoc/auth.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default function (WrappedComponent){
+    return class extends Component {
+        render(){
+            return <WrappedComponent {...this.props} />
+        }
+    }
+}
+
