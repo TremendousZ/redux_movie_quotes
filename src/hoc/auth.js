34de4a934b001b22d2dd5bbddb4ef4bd98ad08bb
John Carlisle <jmcarlisle@live.com>
2018-08-31 15:02:57 -0700

    added user auth

--- src/hoc/auth.js
+++ src/hoc/auth.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 export default function (WrappedComponent){
-    return class extends Component {
+    class Auth extends Component {
+        componentDidMount(){
+           this.checkAuth();
+        }
+
+        componentDidUpdate(){
+            this.checkAuth();
+        }
+
+        checkAuth(){
+            const { auth, history } = this.props;
+            if(!auth){
+                history.push('/');
+            }
+        }
+
         render(){
             return <WrappedComponent {...this.props} />
         }
     }
+
+
+    function mapStateToProps(state){
+        return {
+            auth: state.user.auth
+        }
+    }
+
+    return connect(mapStateToProps)(Auth);
 }
+
+
 
